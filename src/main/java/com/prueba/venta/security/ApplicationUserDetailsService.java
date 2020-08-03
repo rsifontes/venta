0d230c8aaf47fd7b048770cc4e8d6b34128bf6d0
Rafael Sifontes <rafael.sifontes.castroi@gmail.com>
2020-08-03 01:01:56 -0400

    Lógica de busqueda de usuario

---
+++ src/main/java/com/prueba/venta/security/ApplicationUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.prueba.venta.security;
+
+import com.prueba.venta.security.domain.ApplicationUser;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService{
+    private ApplicationUserRepository applicationUserRepository;
+
+    public ApplicationUserDetailsService(ApplicationUserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+        if (applicationUser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(applicationUser.getUsername(), applicationUser.getPassword(), Collections.emptyList());
+    }
+}
---
+++ src/main/java/com/prueba/venta/security/ApplicationUserRepository.java
@@ -0,0 +1,27 @@
+package com.prueba.venta.security;
+
+import com.prueba.venta.security.domain.ApplicationUser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationUserRepository {
+
+    @Value("${username}")
+    public String appUsername;
+    @Value("${password}")
+    public String pass;
+
+    public ApplicationUser findByUsername(String username){
+
+        ApplicationUser user = new ApplicationUser();
+        if(username.equals(appUsername)) {
+            user.setUsername(username);
+            user.setPassword(pass);
+            return user;
+        }
+
+        return null;
+    };
+
+}
