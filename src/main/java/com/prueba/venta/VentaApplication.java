7bebd5e9e05f52cc721fe8f35d63c259d623d8e7
Rafael Sifontes <rafael.sifontes.castroi@gmail.com>
2020-08-02 23:48:15 -0400

    Proyecto inicial

---
+++ src/main/java/com/prueba/venta/VentaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaApplication.class, args);
+	}
+
+}
