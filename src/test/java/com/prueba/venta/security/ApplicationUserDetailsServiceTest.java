781a23fa5905e787b0e8746ec410cf251038788d
Rafael Sifontes <rafael.sifontes.castroi@gmail.com>
2020-08-03 01:36:16 -0400

    Test para Servicio de ApplicationUser

---
+++ src/test/java/com/prueba/venta/security/ApplicationUserDetailsServiceTest.java
@@ -0,0 +1,46 @@
+package com.prueba.venta.security;
+
+import com.prueba.venta.security.domain.ApplicationUser;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ApplicationUserDetailsServiceTest {
+
+    @MockBean
+    ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    ApplicationUserDetailsService applicationUserDetailsService;
+
+    @Test
+    void loadUserByUsernameOk() {
+        ApplicationUser applicationUser = createApplicationUser();
+        when(applicationUserRepository.findByUsername(anyString())).thenReturn(applicationUser);
+        UserDetails user = applicationUserDetailsService.loadUserByUsername("test");
+        Assertions.assertEquals(user.getUsername(), applicationUser.getUsername());
+    }
+
+    @Test
+    void loadUserByUsernameErrorOk() {
+        when(applicationUserRepository.findByUsername(anyString())).thenReturn(null);
+        Assertions.assertThrows(UsernameNotFoundException.class, () ->{
+            applicationUserDetailsService.loadUserByUsername("test");
+        });
+    }
+
+    private ApplicationUser createApplicationUser(){
+        ApplicationUser applicationUser = new ApplicationUser();
+        applicationUser.setPassword("password");
+        applicationUser.setUsername("username");
+        return applicationUser;
+    }
+}
