a10f60be5245ffc98757ddfd37a9ffc4762a626d
Rafael Sifontes <rafael.sifontes.castroi@gmail.com>
2020-08-03 01:53:18 -0400

    Se agregan Test

--- src/main/java/com/prueba/venta/security/ApplicationUserDetailsService.java
+++ src/main/java/com/prueba/venta/security/ApplicationUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import sun.security.krb5.internal.APOptions;
 
 import java.util.Collections;
 
@@ -19,10 +20,21 @@ public class ApplicationUserDetailsService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = buscarApplicationUserPorUsername(username);
+        return generarUserDetail(applicationUser);
+    }
+
+    private ApplicationUser buscarApplicationUserPorUsername( String username) throws UsernameNotFoundException{
         ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
         if (applicationUser == null) {
             throw new UsernameNotFoundException(username);
         }
+        return applicationUser;
+    }
+
+    private UserDetails generarUserDetail(ApplicationUser applicationUser) throws UsernameNotFoundException{
         return new User(applicationUser.getUsername(), applicationUser.getPassword(), Collections.emptyList());
     }
+
+
 }
---
+++ src/test/java/com/prueba/venta/security/ApplicationUserRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.prueba.venta.security;
+
+import com.prueba.venta.security.domain.ApplicationUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class ApplicationUserRepositoryTest {
+
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
+
+    @Test
+    void findByUsername() {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername("userTest");
+        assertEquals(applicationUser.getUsername(),"userTest");
+    }
+
+    @Test
+    void findByUsernameNotFound() {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsername("userNoExiste");
+        assertNull(applicationUser);
+    }
+}
