6cb69cae5bcb27cdad5c78150b3cbc2208f8d21c
Rafael Sifontes <rafael.sifontes.castroi@gmail.com>
2020-08-03 01:02:21 -0400

    Se ajustan constantes

--- src/main/java/com/prueba/venta/security/SecurityConstants.java
+++ src/main/java/com/prueba/venta/security/SecurityConstants.java
@@ -1,4 +1,7 @@
 package com.prueba.venta.security;
 
 public class SecurityConstants {
+    public static final String KEY = "SECRET";
+    public static final String HEADER_NAME = "Authorization";
+    public static final Long EXPIRATION_TIME = 1000L*60*10;
 }
